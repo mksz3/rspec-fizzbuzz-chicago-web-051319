ba750f2e18745e7be8b950e638c0d98447676d60
Markus Sanders <markus.sanderst@gmail.com>
2019-04-22 19:37:29 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  output = ''
+  if int % 3 == 0
+    output += 'Fizz'
+  end
+  if int % 5 == 0
+    output += 'Buzz'
+  end
+  if output == ''
+    return
+  else 
+    return output
+  end
+end
